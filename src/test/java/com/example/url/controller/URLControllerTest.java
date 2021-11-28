29f6c1acb83c7a6886244734cd3977f6c6a32433
demoxie <shadrachadamuul@gmail.com>
2021-11-29 00:44:38 +0100

    make it better

---
+++ src/test/java/com/example/url/controller/URLControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.url.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class URLControllerTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getUrl() {
+    }
+
+    @Test
+    void getUrlHistory() {
+    }
+
+    @Test
+    void create() {
+    }
+}
